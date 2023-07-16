49b79ccf2296a0cf735df09911c58d316f4ff562
tabascum <marques_vasco@yahoo.es>
2023-07-17 01:15:36 +0200

    Update layouts. Add notification dot. Add behavior to _mark all as read_

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const cards = document.querySelectorAll(".nt-card");
 const count = document.querySelector("#header-counter");
 const reset = document.querySelector("#read-marks");
+const dots = document.querySelectorAll(".fa-circle");
 
 let counter = 0;
 const activeColor = "rgb(247, 250, 253)";
@@ -13,4 +14,13 @@ cards.forEach((card) => {
     counter++;
     count.innerText = counter;
   }
+
+  reset.addEventListener("click", () => {
+    counter = 0;
+    card.style.backgroundColor = "transparent";
+    count.innerText = counter;
+    dots.forEach((card) => {
+      card.classList.remove("fa-circle");
+    });
+  });
 });
